11bfaa496a3f0c32d1901c773edc6b665e64fbba
KangAyeon05 <rkddkdus05@gmail.com>
2025-05-21 21:54:30 +0900

    useEffect

---
+++ src/Effect/UserList.jsx
@@ -0,0 +1,28 @@
+import {  useEffect, useState  } from 'react';
+export default function Userlist() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function fetchUsers() {
+            const res = await fetch('https://jsonplaceholder.typicode.com/users');
+            const data = await res.json();
+            setUsers(data);
+            
+        }
+    
+        fetchUsers();
+    }, [])
+
+    return (
+        <div>
+            <h2>사용자 목록</h2>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        <br />{user.name} ({user.email})
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
